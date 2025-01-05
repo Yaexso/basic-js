f23229d4599f8b621c34b7c69dd6524804ca385d
Yaexso <hramykastanislau@gmail.com>
2025-01-05 21:11:49 +0300

    feat: decide encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  if (str.length === 0) {
+    return '';
+  }
+
+  let encoded = '';
+  let count = 1;
+
+  for (let i = 1; i < str.length; i += 1) {
+    if (str[i] === str[i - 1]) {
+      count += 1;
+    } else {
+      if (count > 1) {
+        encoded += count;
+      }
+
+      encoded += str[i - 1];
+      count = 1;
+    }
+  }
+
+  if (count > 1) {
+    encoded += count;
+  }
+
+  return (encoded += str[str.length - 1]);
 }
 
 module.exports = {
